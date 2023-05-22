f8fa3bae0547f313a80cdd009716dc2a26a61844
SvetlanaPanteleeva <123400566+SvetlanaPanteleeva@users.noreply.github.com>
2023-05-22 12:24:22 +0300

    Добавьте файлы проекта.

---
+++ RoomNumerator/MainView.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RoomNumerator
+{ 
+    public partial class MainView : Window
+    {
+        public string NumberPrefix;
+        public string StartFrom;
+        public string SelectedNumberingDirection;
+
+        public MainView()
+        {
+            InitializeComponent();
+        }
+        private void btn_Ok_Click(object sender, RoutedEventArgs e)
+        {
+            NumberPrefix = textBox_NumberPrefix.Text;
+            StartFrom = textBox_StartFrom.Text;
+            SelectedNumberingDirection = (groupBox_NumberingDirection.Content as System.Windows.Controls.Grid)
+                .Children.OfType<RadioButton>()
+                .FirstOrDefault(rb => rb.IsChecked.Value == true)
+                .Name;
+            DialogResult = true;
+            Close();
+        }
+
+        private void btn_Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            Close();
+        }
+        private void RoomNumeratorWPF_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter || e.Key == Key.Space)
+            {
+                NumberPrefix = textBox_NumberPrefix.Text;
+                StartFrom = textBox_StartFrom.Text;
+                SelectedNumberingDirection = (groupBox_NumberingDirection.Content as System.Windows.Controls.Grid)
+                    .Children.OfType<RadioButton>()
+                    .FirstOrDefault(rb => rb.IsChecked.Value == true)
+                    .Name;
+                DialogResult = true;
+                Close();
+            }
+
+            else if (e.Key == Key.Escape)
+            {
+                DialogResult = false;
+                Close();
+            }
+        }
+    }
+}
+
---
+++ RoomNumerator/MainViewViewModel.cs
@@ -0,0 +1,136 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using ClassLibraryItog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomNumerator
+{
+    [Transaction(TransactionMode.Manual)]
+    public class MainViewViewModel: IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            Document doc = commandData.Application.ActiveUIDocument.Document;
+            Selection sel = commandData.Application.ActiveUIDocument.Selection;
+
+            List<Room> roomList = new List<Room>();
+            roomList = GetRoomsFromCurrentSelection(doc, sel);
+
+            if (roomList.Count == 0)
+            {
+                RoomSelectionFilter selFilter = new RoomSelectionFilter();
+                IList<Reference> selRooms = null;
+                try
+                {
+                    selRooms = sel.PickObjects(ObjectType.Element, selFilter, "Выберите помещения!");
+                }
+                catch (Autodesk.Revit.Exceptions.OperationCanceledException)
+                {
+                    return Result.Cancelled;
+                }
+
+                foreach (Reference roomRef in selRooms)
+                {
+                    roomList.Add(doc.GetElement(roomRef) as Room);
+                }
+            }
+
+            //Вызов формы
+            MainView roomNumeratorWPF = new MainView();
+            roomNumeratorWPF.ShowDialog();
+            if (roomNumeratorWPF.DialogResult != true)
+            {
+                return Result.Cancelled;
+            }
+
+            string numberPrefix = roomNumeratorWPF.NumberPrefix;
+            string startFrom = roomNumeratorWPF.StartFrom;
+            bool tryChech = int.TryParse(startFrom, out int cnt);
+            if (!tryChech) cnt = 1;
+
+            string selectedNumberingDirection = roomNumeratorWPF.SelectedNumberingDirection;
+            switch (selectedNumberingDirection)
+            {
+                case "radioButton_RightAndDown":
+                    //Вправо и вниз
+                    roomList = roomList.OrderByDescending(r => GetRoomCenter(r)
+                        , new CoordinatesBasedComparerXYDown())
+                        .ToList();
+                    break;
+                case "radioButton_DownAndRight":
+                    //Вниз и вправо
+                    roomList = roomList.OrderByDescending(r => GetRoomCenter(r)
+                        , new CoordinatesBasedComparerYXDown())
+                        .ToList();
+                    break;
+                case "radioButton_RightAndUp":
+                    //Вправо и вверх
+                    roomList = roomList.OrderByDescending(r => GetRoomCenter(r)
+                        , new CoordinatesBasedComparerXYUp())
+                        .ToList();
+                    break;
+                case "radioButton_UpAndRight":
+                    //Вверх и вправо
+                    roomList = roomList.OrderByDescending(r => GetRoomCenter(r)
+                        , new CoordinatesBasedComparerYXUp())
+                        .ToList();
+                    break;
+            }
+
+            using (Transaction t = new Transaction(doc))
+            {
+                t.Start("Нумерация помещений");
+
+                foreach (Room room in roomList)
+                {
+                    if (numberPrefix == "" || numberPrefix == null)
+                    {
+                        room.get_Parameter(BuiltInParameter.ROOM_NUMBER).Set($"{cnt}");
+                    }
+                    else
+                    {
+                        room.get_Parameter(BuiltInParameter.ROOM_NUMBER).Set($"{numberPrefix}{cnt}");
+                    }
+                    cnt++;
+                }
+
+                t.Commit();
+            }
+
+            return Result.Succeeded;
+        }
+        private static List<Room> GetRoomsFromCurrentSelection(Document doc, Selection sel)
+        {
+            ICollection<ElementId> selectedIds = sel.GetElementIds();
+            List<Room> tempRoomsList = new List<Room>();
+            foreach (ElementId roomId in selectedIds)
+            {
+                if (doc.GetElement(roomId) is Room
+                    && null != doc.GetElement(roomId).Category
+                    && doc.GetElement(roomId).Category.Id.IntegerValue.Equals((int)BuiltInCategory.OST_Rooms))
+                {
+                    tempRoomsList.Add(doc.GetElement(roomId) as Room);
+                }
+            }
+            return tempRoomsList;
+        }
+
+        private static XYZ GetRoomCenter(Room room)
+        {
+            XYZ tmpXYZ = null;
+            tmpXYZ = (room.get_BoundingBox(null).Max + room.get_BoundingBox(null).Min) / 2;
+            return tmpXYZ;
+        }
+
+       
+    }
+}
+    
+
